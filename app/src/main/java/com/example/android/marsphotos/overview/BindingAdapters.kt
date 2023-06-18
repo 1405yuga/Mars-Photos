96d6a6443097aa2db418930fcd252db9e11040f5
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-06-18 15:14:08 +0530

    adding bindingAdapter

---
+++ app/src/main/java/com/example/android/marsphotos/overview/BindingAdapters.kt
@@ -0,0 +1,13 @@
+package com.example.android.marsphotos.overview
+
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+
+
+class BindingAdapters() {
+
+    @BindingAdapter("imageUrl")
+    fun bindImage(imgView: ImageView, imgUrl: String?) {
+
+    }
+}
